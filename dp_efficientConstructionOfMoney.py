3bdc092a224b04c5e984384e88baa5dae7c1a2e3
heewoo <tigerson95@gmail.com>
2021-03-19 20:21:44 +0900

    dynamic programming 효율적인 화폐 구성 문제

---
+++ dp_efficientConstructionOfMoney.py
@@ -0,0 +1,18 @@
+n, m = max(int, input().split())
+array = []
+for i in range(n):
+    array.append(int(input()))
+
+d = [10001] * (m + 1)
+
+# bottom up
+d[0] = 0
+for i in range(n):
+    for j in range(array[i], m + 1):
+        if d[j - array[i]] != 10001: # (i - k)원을 만드는 방법이 존재하는 경우
+            d[j] = min(d[j], d[j - array[i]] + 1)
+
+if d[m] == 10001: # 최종적으로 M원을 만드는 방법이 없는 경우
+    print(-1)
+else:
+    print(d[m])
